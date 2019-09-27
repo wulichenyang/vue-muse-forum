967148b46c00ba2fde53e785d81e1f4d772de1f2
lichenyang <wulichenyang@163.com>
2019-09-27 20:28:12 +0800

    :art: Draw UploadPhoto from AdminCategoryAdd.vue

--- src/views/Admin/AdminCategory/AdminCategoryAdd.vue
+++ src/views/Admin/AdminCategory/AdminCategoryAdd.vue
@@ -10,24 +10,10 @@
       >
         <!-- 头像上传 -->
         <mu-form-item label="上传分类图片">
-
-          <el-upload
-            class="avatar-uploader"
-            :action="domain"
-            :http-request="uploadQiniu"
-            :show-file-list="false"
-            :before-upload="beforeUpload"
-          >
-            <img
-              v-if="form.avatar"
-              :src="form.avatar"
-              class="avatar"
-            >
-            <i
-              v-else
-              class="el-icon-plus avatar-uploader-icon"
-            ></i>
-          </el-upload>
+          <UploadPhoto
+            v-model="form.avatar"
+            :ifSubmit="ifSubmit"
+          />
         </mu-form-item>
 
         <!-- 分类名 -->
@@ -85,41 +71,26 @@ import {
   Vue,
   Prop,
   Emit,
-  Model,
-  Watch
 } from "vue-property-decorator";
 import FormWrapper from "@/components/FormWrapper.vue";
+import UploadPhoto from "@/components/Upload/UploadPhoto.vue";
 import { Getter, Action } from "vuex-class";
 import {} from "@/assets/js/dataType";
 import To from "@/utils/to";
 import Toast from "muse-ui-toast";
-import { qiniuConfig } from "@/config/index";
-import { fetchQiniuToken, uploadPhoto, deletePrePhoto } from "@/api/upload";
 import { addCategory } from "@/api/category";
 import { CategoryPayload } from "@/api/category";
 import { categoryNameRules, categoryBriefRules } from "@/utils/validate";
 
 @Component({
   components: {
-    FormWrapper
+    FormWrapper,
+    UploadPhoto
   }
 })
 // TODO: copper
 export default class AdminCategoryAdd extends Vue {
-  // Props
-  // @Prop({
-  //   default: [],
-  //   required: true,
-  // })
-  // list!: any;
-
-  // @Model("onChange", {
-  //   type: String
-  // })
-  // searchKey!: string;
-
   // Data
-
   labelPosition: string = "top";
   form: any = {
     categoryName: "",
@@ -128,15 +99,7 @@ export default class AdminCategoryAdd extends Vue {
     // 图片路径
     avatar: ""
   };
-  // 提交表单时，图片是否已上传成功
-  isUploadAvatarOk: boolean = false;
-  // 图片纯key值，用于删除图片
-  photoKey: string = "";
-  token: any = {};
-  // 七牛云的上传地址，根据自己所在地区选择，我这里是华南区
-  domain: string = qiniuConfig.domain;
-  // 这是七牛云空间的外链默认域名
-  qiniuaddr: string = qiniuConfig.qiniuAddr;
+
   // 是否已提交过表单
   ifSubmit: boolean = false;
   // 提交状态标志
@@ -146,115 +109,15 @@ export default class AdminCategoryAdd extends Vue {
   categoryNameRules: any = categoryNameRules;
   categoryBriefRules: any = categoryBriefRules;
 
-  // Computed
-  // get computedData() {
-  //   return ' cc' + this.searchValue;
-  // }
-
   // Lifecycle
-  mounted() {
-    // 添加刷新页面监听
-    window.addEventListener("beforeunload", e => this.beforeunloadFn(e));
-  }
+  mounted() {}
 
-  // 如果该表单没提交，则删除缓存的图片
-  beforeDestroy() {
-    if (this.photoKey && !this.ifSubmit) {
-      deletePrePhoto(this.photoKey);
-    }
-  }
-
-  // 卸载页面监听事件
-  destroyed() {
-    window.removeEventListener("beforeunload", e => this.beforeunloadFn(e));
+  // 更新当前avatarUrl
+  onAvatarChange(avatarUrl: string) {
+    this.form.avatar = avatarUrl;
   }
 
   // Methods
-
-  // 监听刷新或关闭时删除缓存图片
-  beforeunloadFn(e: any) {
-    console.log("刷新或关闭");
-    if (this.photoKey && !this.ifSubmit) {
-      deletePrePhoto(this.photoKey);
-    }
-  }
-
-  async uploadQiniu(req: any) {
-    console.log(req);
-    const uploadConfig = {
-      headers: { "Content-Type": "multipart/form-data" }
-    };
-
-    let filetype = "";
-    if (req.file.type === "image/png") {
-      filetype = "png";
-    } else {
-      filetype = "jpg";
-    }
-    // 重命名要上传的文件
-    const keyname =
-      "category" +
-      "-" +
-      new Date().getTime() +
-      "-" +
-      Math.floor(Math.random() * 100) +
-      "." +
-      filetype;
-    // 从后端获取上传凭证token
-    let err, token;
-    [err, token] = await To(fetchQiniuToken());
-    // 获取token错误
-    if (err) {
-      return;
-    }
-    if (token && token.code === 0) {
-      console.log(token);
-      // 构建FormData图片对象
-      const formdata = new FormData();
-      formdata.append("file", req.file);
-      formdata.append("token", token.data);
-      formdata.append("key", keyname);
-      // 获取到凭证之后再将文件上传到七牛云空间
-      let uploadRes;
-      // Attention！：跨域，需要配置代理
-      [err, uploadRes] = await To(uploadPhoto(formdata, uploadConfig));
-      if (err) {
-        return;
-      }
-      // 上传七牛云成功，获取图片路径
-      if (uploadRes && uploadRes.key) {
-        console.log(uploadRes);
-        if (this.photoKey) {
-          // 多次上传，清除之前上传的图片
-          deletePrePhoto(this.photoKey);
-        }
-        this.photoKey = uploadRes.key;
-        this.form.avatar = "http://" + this.qiniuaddr + "/" + uploadRes.key;
-        this.isUploadAvatarOk = true;
-        console.log("avatar:    ", this.form.avatar);
-      }
-    }
-  }
-
-  // 验证文件合法性
-  beforeUpload(file: File): boolean {
-    const isJPG = file.type === "image/jpeg" || file.type === "image/png";
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isJPG) {
-      Toast.error("上传头像图片只能是 JPG 格式!");
-    }
-    if (!isLt2M) {
-      Toast.error("上传头像图片大小不能超过 2MB!");
-    }
-    // 检测通过
-    if (isJPG && isLt2M) {
-      this.isUploadAvatarOk = false;
-      return true;
-    }
-    // 检测失败
-    return false;
-  }
-
   clearForm() {
     this.form = {
       categoryName: "",
@@ -315,40 +178,12 @@ export default class AdminCategoryAdd extends Vue {
 
   // @Action("getUser") getUser: any;
 
-  // @Emit("select")
-  // select(listItem: Song, index: number) {}
-
-  // @Watch("child", { immediate: true, deep: true })
-  // onChildChanged(val: string, oldVal: string) {}
 }
 </script>
 
 <style lang="scss">
 @import "../../../assets/css/var.scss";
 .admin-category-add {
-  .avatar-uploader .el-upload {
-    margin-top: 20px;
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: $primary;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: $avatarSize;
-    height: $avatarSize;
-    line-height: $avatarSize;
-    text-align: center;
-  }
-  .avatar {
-    width: $avatarSize;
-    height: $avatarSize;
-    display: block;
-  }
+
 }
 </style>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -52,6 +52,20 @@
           >
           </quill-editor>
 
+          <!--饿了么上传组件，也可以用input代替，都是隐藏起来手动监听图片事件，上传七牛云-->
+          <el-upload
+            class="avatar-uploader"
+            :action="domain"
+            :http-request="uploadQiniu"
+            :show-file-list="false"
+            style="display: none"
+          >
+            <i
+              class="el-icon-plus avatar-uploader-icon"
+              ref="imgInput"
+            ></i>
+          </el-upload>
+
         </section>
         <mu-button
           v-loading="submitting"
@@ -82,8 +96,12 @@ import ContainerInner from "@/components/ContainerInner.vue";
 import { quillEditor } from "vue-quill-editor";
 import { postTitleRules, categoryNameRules } from "@/utils/validate";
 import { PostState, PostPayload, addPost } from "@/api/post";
+import { fetchQiniuToken, uploadPhoto } from "@/api/upload";
+import { qiniuConfig } from "@/config/index";
 import Toast from "muse-ui-toast";
 import To from "@/utils/to";
+import Quill from 'quill'
+
 @Component({
   components: {
     ContainerInner,
@@ -116,24 +134,131 @@ export default class NewPost extends Vue {
   // 表单提交中
   submitting: boolean = false;
   colorArray: string[] = colorArray;
+  token: string = "";
+  // 七牛云的上传地址，根据自己所在地区选择，我这里是华南区
+  domain: string = qiniuConfig.domain;
+  // 这是七牛云空间的外链默认域名
+  qiniuaddr: string = qiniuConfig.qiniuAddr;
+
   // categoryNow: CategoryDetail | {} = {};
   // 正则验证
   postTitleRules: any = postTitleRules;
   categoryNameRules: any = categoryNameRules;
+  // vue-quill-editor 富文本内容变化区域，用于图片上传
+  addRange: any;
+
   // Computed
   // get computedData() {
   //   return ' cc' + this.searchValue;
   // }
 
   // Lifecycle
-  mounted() {
+  async mounted() {
     // Vuex为空，重新请求数据
     if (this.categoryIds.length === 0) {
       this.getCategories();
     }
+    // 监听富文本上传图片动作
+    this.listenInsertPhoto();
   }
 
   // Methods
+  listenInsertPhoto() {
+    // 为图片ICON绑定事件  getModule 为编辑器的内部属性
+    (this.$refs.myTextEditor as any).quill
+      .getModule("toolbar")
+      .addHandler("image", this.imgHandler);
+  }
+
+  // 选择图片时自动上传七牛云
+  imgHandler(state: any) {
+    console.log(state);
+    this.addRange = (this.$refs.myTextEditor as any).quill.getSelection();
+    if (state) {
+      // let fileInput = document.getElementById('imgInput')
+      // fileInput.click() // 加一个触发事件
+      // 选择并上传一张图片
+      let fileInput = this.$refs.imgInput;
+      (fileInput as any).click(); // 加一个触发事件
+    }
+  }
+
+  async uploadQiniu(req: any) {
+    console.log(req);
+    const uploadConfig = {
+      headers: { "Content-Type": "multipart/form-data" }
+    };
+
+    let filetype = "";
+    if (req.file.type === "image/png") {
+      filetype = "png";
+    } else {
+      filetype = "jpg";
+    }
+
+    // 重命名要上传的文件
+    const keyname =
+      "post" +
+      "-" +
+      new Date().getTime() +
+      "-" +
+      Math.floor(Math.random() * 100) +
+      "." +
+      filetype;
+    // 从后端获取上传凭证token
+    let err, token;
+    [err, token] = await To(fetchQiniuToken());
+    // 获取token错误
+    if (err) {
+      return;
+    }
+
+    if (token && token.code === 0) {
+      console.log(token);
+      // 构建FormData图片对象
+      const formdata = new FormData();
+      formdata.append("file", req.file);
+      formdata.append("token", token.data);
+      formdata.append("key", keyname);
+      // 获取到凭证之后再将文件上传到七牛云空间
+      let uploadRes;
+      // Attention！：跨域，需要配置代理
+      [err, uploadRes] = await To(uploadPhoto(formdata, uploadConfig));
+      if (err) {
+        return;
+      }
+      // 上传七牛云成功，获取图片路径
+      if (uploadRes && uploadRes.key) {
+        console.log(uploadRes);
+
+        // 图片的远程路径
+        let photoUrl = "http://" + this.qiniuaddr + "/" + uploadRes.key;
+        // 将图片添加到富文本内容区域
+        this.addRange = (this.$refs.myTextEditor as any).quill.getSelection();
+        // 调用编辑器的 insertEmbed 方法，插入URL
+        (this.$refs.myTextEditor as any).quill.insertEmbed(
+          this.addRange !== null ? this.addRange.index : 0,
+          "image",
+          photoUrl,
+          Quill.sources.USER
+        );
+      }
+    }
+  }
+
+  async getToken() {
+    // 从后端获取上传凭证token
+    let err, token;
+    [err, token] = await To(fetchQiniuToken());
+    // 获取token错误
+    if (err) {
+      return;
+    }
+    if (token && token.code === 0) {
+      this.token = token.data;
+    }
+  }
+
   // 富文本编辑器
   onEditorChange({ editor, html, text }: any) {
     // console.log('editor change!', editor, html, text)
